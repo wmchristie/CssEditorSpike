17ea0d4b44688730d4925c24520cf587b86af023
Bill Christie <william.christie@gmail.com>
2012-04-07 11:18:51 -0400

    Project inception

---
+++ CssEditorSpike/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace CssEditorSpike.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CssEditorSpikeSpecs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CssEditorSpikeSpecs.MvcApplication" Language="C#" %>
