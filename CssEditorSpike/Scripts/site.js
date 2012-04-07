24fa37146238b272bbe03ae6ff246e4f786da3d9
Bill Christie <william.christie@gmail.com>
2012-04-07 16:25:16 -0400

    Added jquery 1.7.1 and underscore.js packages. Added js and css references to the _layout.cshtml.

---
+++ CssEditorSpike/Scripts/site.js
@@ -0,0 +1,14 @@
+﻿/*global CodeMirror _ */
+(function (app, $, _, cm, undef) {
+    'use strict';
+
+    app.initialize = function () {
+        console.log('foo');
+    };
+
+    $(function () {
+        app.initialize();
+    });
+
+
+}((window.app = {}), jQuery, _, CodeMirror));
